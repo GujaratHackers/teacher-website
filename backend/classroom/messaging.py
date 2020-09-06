cc51ac7ce88a795260fc8798eb16dad149f17cdd
Ajayneethikannan <ajaykarate001@gmail.com>
2020-09-06 14:57:43 +0530

    Replace aws by twilio for now

--- backend/classroom/messaging.py
+++ backend/classroom/messaging.py
@@ -1,13 +1,32 @@
 import boto3
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+
+account_sid = "AC72bf334a5108bf91e562ca51d8154e95"
+auth_token = "93b796df46556d85773b533b495c33d9"
+twilio_client = Client(account_sid, auth_token)
+
 
 def initiate_client():
 
     client = boto3.client(
         "sns",
-        aws_access_key_id = 'ASIASWREH62U5467WSMR',
-        aws_secret_access_key = 'eTdCtZ2i5CFG+bvuHb5XaGIbNJ2x4E6mEBYNpamE',
-        aws_session_token = 'FwoGZXIvYXdzEAAaDMWwv9nW57mOJYBnHSK/AW7KVNuJytdgcCHswfJ+PfujDRAiOTaFjvtOQvVkH9zLsZ9Z8ktWDLvWI6WQiLc3U7gEwzVXtpYdW7zf/3XepJ9na+00iqaO56BqLu0o31sAzvqXjSyZbcKXMg0ccV0DVlSlq4fa9aLmwKke81XOx9YxuGlSqxfQ7bUB53nsvuRLsb2U6fgXaxgvOTzPRH8aJhogd45mqu6tHPt+9Uib1dvg8fLTxUvcyTkVD4761IJAfm5qJ43SEJSltUHaJlD/KLmI0voFMi2XlL1jjVoLH4gm03o7oGQy+TM4Xe0G5qYmpscHyp/x5vTr6ysTQFXKgVyrvmk=',
-        region_name = "us-east-1"
+        aws_access_key_id="ASIASWREH62U5467WSMR",
+        aws_secret_access_key="eTdCtZ2i5CFG+bvuHb5XaGIbNJ2x4E6mEBYNpamE",
+        aws_session_token="FwoGZXIvYXdzEAAaDMWwv9nW57mOJYBnHSK/AW7KVNuJytdgcCHswfJ+PfujDRAiOTaFjvtOQvVkH9zLsZ9Z8ktWDLvWI6WQiLc3U7gEwzVXtpYdW7zf/3XepJ9na+00iqaO56BqLu0o31sAzvqXjSyZbcKXMg0ccV0DVlSlq4fa9aLmwKke81XOx9YxuGlSqxfQ7bUB53nsvuRLsb2U6fgXaxgvOTzPRH8aJhogd45mqu6tHPt+9Uib1dvg8fLTxUvcyTkVD4761IJAfm5qJ43SEJSltUHaJlD/KLmI0voFMi2XlL1jjVoLH4gm03o7oGQy+TM4Xe0G5qYmpscHyp/x5vTr6ysTQFXKgVyrvmk=",
+        region_name="us-east-1",
     )
 
     return client
+
+
+def send_message(phone_number, message):
+    """
+    Send a message using twilio client
+    """
+    try:
+        twilio_client.messages.create(
+            to="+91" + phone_number, from_="+12058756630", body=message
+        )
+    except:
+        pass
--- backend/classroom/views.py
+++ backend/classroom/views.py
@@ -11,7 +11,9 @@ from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 
-from .messaging import initiate_client
+from .messaging import initiate_client, send_message
+
+from threading import Timer
 
 from .serializers import (
     SignUpSerializer,
@@ -21,9 +23,18 @@ from .serializers import (
     QuizSerializer,
     QuizDetailSerializer,
     QuestionSerializer,
-    StudyMaterialSerializer
+    StudyMaterialSerializer,
 )
-from .models import Student, Teacher, Class, Quiz, Question, StudyMaterial, AnswerSheet, Answer
+from .models import (
+    Student,
+    Teacher,
+    Class,
+    Quiz,
+    Question,
+    StudyMaterial,
+    AnswerSheet,
+    Answer,
+)
 
 
 account_sid = "AC72bf334a5108bf91e562ca51d8154e95"
@@ -38,6 +49,7 @@ twilio_client = Client(account_sid, auth_token)
 
 # print(message.sid)
 aws_client = initiate_client()
+
 
 class SignUp(generics.GenericAPIView):
     """
@@ -78,6 +90,7 @@ class StudentViewset(viewsets.ModelViewSet):
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
     permission_classes = [IsAuthenticated]
+
 
 class QuestionViewset(viewsets.ModelViewSet):
     """
@@ -126,15 +139,12 @@ class ClassroomViewset(viewsets.ModelViewSet):
         endpoint = student.phone_number
         cname = student_class.name
 
-
         # aws_client.publish(
         # PhoneNumber='+91'+endpoint,
         # Message='CLASS>'+cname)
 
         twilio_client.messages.create(
-            body='CLASS>' + cname,
-            from_='+12058756630',
-            to='+91'+endpoint
+            body="CLASS>" + cname, from_="+12058756630", to="+91" + endpoint
         )
         s = self.get_serializer(student_class)
 
@@ -159,13 +169,16 @@ class ClassroomViewset(viewsets.ModelViewSet):
 
 class QuizViewset(viewsets.ModelViewSet):
     def get_serializer_class(self):
-        if self.action == "retrieve" or self.request.query_params.get("quiz_id", None) != None:
-            return QuizDetailSerializer # Returns all the information in the case of viewing answers
+        if (
+            self.action == "retrieve"
+            or self.request.query_params.get("quiz_id", None) != None
+        ):
+            return QuizDetailSerializer  # Returns all the information in the case of viewing answers
         return QuizSerializer
 
     def get_queryset(self):
         user = self.request.user
-        if(self.request.query_params.get("quiz_id", None) != None):
+        if self.request.query_params.get("quiz_id", None) != None:
             return Quiz.objects.filter(id=self.request.query_params["quiz_id"])
         class_id = self.request.query_params["class_id"]
         class_name = Class.objects.get(id=class_id)
@@ -180,45 +193,57 @@ class QuizViewset(viewsets.ModelViewSet):
         for question in self.request.data["question_list"]:
             qobj = Question.objects.create(detail=question, quiz=instance)
             qobj.save()
-        
-        # client = initiate_client()
 
+        # client = initiate_client()
+        quiz_instance = instance
         registered_students = class_name.students.all()
         cname = class_name.name
         questions = Question.objects.filter(quiz=instance)
-        questions.order_by('id')
+        questions.order_by("id")
+
+        # Send the starting message to the students
+        starter_message = "QUIZ>" + cname + '|' + quiz_instance.name
+        for registered_student in registered_students:
+
+            phno = "+91" + registered_student.phone_number
+            # aws_client.publish(PhoneNumber=phno, Message=message)
+            print(phno)
+            twilio_client.messages.create(
+                body=starter_message, from_="+12058756630", to=phno
+            )
 
         i = 0
         for question in questions:
-            i+=1
+            i += 1
             print(question, i)
             quest = question.detail
             quiz_name = question.quiz.name
-            message = 'QUIZ>' + cname + '|' + quiz_name + '|' + str(i) + '|' + quest
+            message = "QUIZ>" + cname + "|" + quiz_name + "|" + str(i) + "|" + quest
 
             for registered_student in registered_students:
-                phno = '+91' + registered_student.phone_number
+                phno = "+91" + registered_student.phone_number
                 # aws_client.publish(PhoneNumber=phno, Message=message)
 
                 twilio_client.messages.create(
-                    body=message,
-                    from_='+12058756630',
-                    to=phno
+                    send_messmessage, from_="+12058756630", to=phno
                 )
+
 
 class AnswersheetViewset(viewsets.ModelViewSet):
     def get_serializer_class(self):
-        return 
+        return
+
 
 class StudyMaterialViewset(viewsets.ModelViewSet):
     serializer_class = StudyMaterialSerializer
+
     def get_queryset(self):
         try:
-            class_id = self.request.query_params['class_id']
+            class_id = self.request.query_params["class_id"]
         except:
             return []
         return StudyMaterial.objects.filter(class_name=class_id)
-    
+
     def perform_create(self, serializer):
         """
         Override the create method to attach the class and also send the sms
@@ -226,10 +251,33 @@ class StudyMaterialViewset(viewsets.ModelViewSet):
         class_room = Class.objects.get(id=self.request.data["class_id"])
         instance = serializer.save(class_name=class_room)
 
-        study_material = instance # For creating sms messages from the instance
+        
+        
+        
+        
+        study_material = instance  # For creating sms messages from the instance
+        
+        topic = study_material.topic
+        detail = study_material.detail
+
+        topic = topic[:100] # Restrict the topic string size
+        chunks = [detail[i:i+100] for i in range(0, len(detail), 100)] # Divide details into chunks of size 100
+
+        file_message_header = "FILE>" + class_room.name + "|" + topic
+
+        for student in class_room.students.all():
+            phone_number = student.phone_number
+            send_message(phone_number, file_message_header)
+        
+        i=0
+        for chunk in chunks:
+            for student in class_room.students.all():
+                phone_number = student.phone_number
+                send_message(phone_number, file_message_header + "|" + str(i) + "|" + chunk)
+            i = i+1
 
 
-@api_view(('GET', 'POST'))
+@api_view(("GET", "POST"))
 @renderer_classes((JSONRenderer,))
 def receive_sms(request):
     """
@@ -237,16 +285,16 @@ def receive_sms(request):
     """
     print(request.data)
     resp = MessagingResponse()
-    message = request.data['Body']
-    from_number = request.data['From'][3:] # Ignore +91
+    message = request.data["Body"]
+    from_number = request.data["From"][3:]  # Ignore +91
     print(from_number)
     students = Student.objects.filter(phone_number=from_number)
     student = students[0]
     # Need to figure out quiz id, question ids, and answers
-    parts = message.split('|')
+    parts = message.split("|")
     quiz_name = parts[1]  # The quiz name is the second part
-    class_name = parts[0] # The class name is first part
-    answers = parts[2:] # From 2, the rest are answers
+    class_name = parts[0]  # The class name is first part
+    answers = parts[2:]  # From 2, the rest are answers
 
     try:
         quiz_instance = Quiz.objects.get(class_name__name=class_name, name=quiz_name)
@@ -254,11 +302,11 @@ def receive_sms(request):
         return Response(str(resp))
 
     questions = Question.objects.filter(quiz=quiz_instance)
-    questions.order_by('id') # Order them by the creation id
+    questions.order_by("id")  # Order them by the creation id
 
     answersheet = AnswerSheet.objects.create(student=student, quiz=quiz_instance)
     answersheet.save()
-    i=0
+    i = 0
     for answer in answers:
         # question_id = ''
         print(answer, questions[i])
